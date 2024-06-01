b766ab16e7f64b8c59bb05f31c43a3e970d4abe2
wlz2022 <2667461198@qq.com>
2024-06-01 17:23:47 +0800

    test we4

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! 路由可以的~！！！！" />
+      <HelloWorld msg="You did it! 路由可以的~！！！！呀" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
